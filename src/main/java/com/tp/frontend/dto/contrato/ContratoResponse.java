b3acda6e0100f6088a26f99e1e9a277eb7569eea
MaEugeniaDev <meugeniabarca@gmail.com>
2026-02-12 01:53:53 -0300

    vista contratos

--- src/main/java/com/tp/frontend/dto/contrato/ContratoResponse.java
+++ src/main/java/com/tp/frontend/dto/contrato/ContratoResponse.java
@@ -5,14 +5,21 @@ import java.time.LocalDate;
 public class ContratoResponse {
 
     private Long id;
+    private String codigo;
     private LocalDate fechaContrato;
     private Boolean conArma;
-
     private Long sucursalId;
+    private String sucursalCodigo;
     private Long vigilanteId;
+    private String vigilanteCodigo;
+
+
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
+    public String getCodigo() { return codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
 
     public LocalDate getFechaContrato() { return fechaContrato; }
     public void setFechaContrato(LocalDate fechaContrato) { this.fechaContrato = fechaContrato; }
@@ -23,6 +30,22 @@ public class ContratoResponse {
     public Long getSucursalId() { return sucursalId; }
     public void setSucursalId(Long sucursalId) { this.sucursalId = sucursalId; }
 
+    public String getSucursalCodigo() {
+        return sucursalCodigo;
+    }
+    public void setSucursalCodigo(String sucursalCodigo) {
+        this.sucursalCodigo = sucursalCodigo;
+    }
+
     public Long getVigilanteId() { return vigilanteId; }
     public void setVigilanteId(Long vigilanteId) { this.vigilanteId = vigilanteId; }
+
+    public String getVigilanteCodigo() {
+        return vigilanteCodigo;
+    }
+    public void setVigilanteCodigo(String vigilanteCodigo) {
+        this.vigilanteCodigo = vigilanteCodigo;
+    }
+
+
 }
