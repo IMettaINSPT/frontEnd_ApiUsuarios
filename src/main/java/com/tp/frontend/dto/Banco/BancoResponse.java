60555523f63aa076c917649a0f944b81922a5d21
MaEugeniaDev <meugeniabarca@gmail.com>
2026-02-14 04:14:28 -0300

    banco ahora muestra sucursales

--- src/main/java/com/tp/frontend/dto/Banco/BancoResponse.java
+++ src/main/java/com/tp/frontend/dto/Banco/BancoResponse.java
@@ -1,9 +1,15 @@
 package com.tp.frontend.dto.Banco;
+import java.util.List;
+import com.tp.frontend.dto.Sucursal.SucursalResponse; //  importar DTO de sucursal
 
 public class BancoResponse {
     private Long id;
     private String codigo;
     private String domicilioCentral;
+    private Long sucursalId;
+    private String sucursalCodigo;
+    // Lista
+    private List<SucursalResponse> sucursales;
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -13,4 +19,28 @@ public class BancoResponse {
 
     public String getDomicilioCentral() { return domicilioCentral; }
     public void setDomicilioCentral(String domicilioCentral) { this.domicilioCentral = domicilioCentral; }
+
+    public Long getSucursalId() {
+        return sucursalId;
+    }
+    public void setSucursalId(Long bancoId) {
+        this.sucursalId = sucursalId;
+    }
+
+    public String getSucursalCodigo() {
+        return sucursalCodigo;
+    }
+    public void setSucursalCodigo(String sucursalCodigo) {
+        this.sucursalCodigo = sucursalCodigo;
+    }
+
+    // Getter y Setter para la lista
+    public List<SucursalResponse> getSucursales() {
+        return sucursales;
+    }
+    public void setSucursales(List<SucursalResponse> sucursales) {
+        this.sucursales = sucursales;
+    }
+
+
 }
