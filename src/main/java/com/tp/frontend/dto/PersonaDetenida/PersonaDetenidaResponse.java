26d752715fa56b54af020892e0e99365d965a552
MaEugeniaDev <meugeniabarca@gmail.com>
2026-02-19 22:50:59 -0300

    pers detenida 2

--- src/main/java/com/tp/frontend/dto/PersonaDetenida/PersonaDetenidaResponse.java
+++ src/main/java/com/tp/frontend/dto/PersonaDetenida/PersonaDetenidaResponse.java
@@ -1,11 +1,14 @@
 package com.tp.frontend.dto.PersonaDetenida;
 
 import com.tp.frontend.dto.Banda.BandaResponse;
+import java.util.List;
+import com.tp.frontend.dto.Asalto.AsaltoResponse;
 
 public record PersonaDetenidaResponse(
         Long id,
         String codigo,
         String nombre,
         String apellido,
-        BandaResponse banda
+        BandaResponse banda,
+        List<AsaltoResponse> asaltos
 ) {}
